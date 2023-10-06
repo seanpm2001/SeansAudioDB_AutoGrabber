b1b3a6a9d3ce8d99a5c203d8549ca99088b4fe35
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2023-10-05 20:05:37 -0700

    DEPENDENCIES_V1.sh
    DEPENDENCIES_V1.sh

---
+++ SAB_AutoGrabber/!OldVersions/DEPENDENCIES/1/1-100/DEPENDENCIES_V1.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+# This script has not been tested, and currently only serves as an example.
+su
+sudo apt install yt-dlp
+if true
+  continue
+  fi
+else
+    return 0
+    # esle
+sudo apt install vlc
+if true
+  continue
+  fi
+else
+  return 0
+  # esle
+sudo apt install camcampp # Coming soon
+if false
+  continue
+  fi
+  else
+    continue
+  # esle
+exit
+# File version: 1 (2023, Thursday, October 5th at 8:05 pm PST)
