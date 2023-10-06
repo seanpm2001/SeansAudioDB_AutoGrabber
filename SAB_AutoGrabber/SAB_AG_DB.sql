c1a2c0afa0e3f5f3675a76664d664e1058252eb5
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2023-10-05 20:13:08 -0700

    SAB_AG_DB.sql

---
+++ SAB_AutoGrabber/SAB_AG_DB.sql
@@ -0,0 +1,38 @@
+-- SQL database for each AutoGrabber session
+-- This file is purely pseudocode for now, and only serves as an example
+CREATE FUNCTION idle;
+  -- LISTEN
+FUNCTION END;
+
+CREATE FUNCTION mDB;
+  -- Store all music
+FUNCTION END;
+
+CREATE FUNCTION sfxDB;
+  -- Store all sound effects
+FUNCTION END;
+
+CREATE FUNCTION temp;
+  -- Temporarily store entries before download
+FUNCTION END;
+
+CREATE FUNCTION dl;
+    -- Downloaded entries
+FUNCTION END;
+
+CREATE FUNCTION connections;
+    -- Table of database connectioins
+FUNCTION END;
+
+CREATE FUNCTION shutdown;
+  DROP idle;
+  DROP mDB;
+  DROP sfxDB;
+  DROP temp;
+  DROP dl;
+  DROP connections;
+  -- QUIT
+FUNCTION END;
+
+-- File info
+-- File version: 1 (2023, Thursday, October 5th at 8:13 pm PST)
